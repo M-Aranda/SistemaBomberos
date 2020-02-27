a487106b4639e84f05f68dc85f072e37c6ecf954
Marcelo <cheloz_20@hotmail.com>
2020-02-27 16:53:42 -0300

    cambio menor

--- Bomberos/bitacoraUnidad.php
+++ Bomberos/bitacoraUnidad.php
@@ -29,8 +29,6 @@
   <head>
     <meta charset="utf-8">
     <title>Mantenedor</title>
-
-
     <link rel ="stylesheet" href="css/style.css" type="text/css">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
